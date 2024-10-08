8e7ca40f2a9284fe369bbc9240333cf3fae0fdf5
Dakota Chanthakoummane <chanzay.c@gmail.com>
2024-10-08 12:28:49 -0400

    Centered the calculator

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
 
 
   return (
-    <div className='w-auto h-auto p-2 flex justify-center'>
+    <div className='w-auto h-auto p-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
     <Buttons/>
     </div>
   )
